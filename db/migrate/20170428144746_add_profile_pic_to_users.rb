be662c4a3981acbd9c35b160431609c643706393
Ryan Wilkinson <charlesryanwilkinson@gmail.com>
2017-04-29 14:23:53 -0400

    default profile pic

--- db/migrate/20170428144746_add_profile_pic_to_users.rb
+++ db/migrate/20170428144746_add_profile_pic_to_users.rb
@@ -1,5 +1,5 @@
 class AddProfilePicToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :profile_pic, :string
+    add_column :users, :profile_pic, :string, :default => "http://www.guided-selling.org/wp-content/uploads/default.png"
   end
 end
