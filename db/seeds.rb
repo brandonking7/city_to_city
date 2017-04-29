c6435e322c605ad77267f4c90a5d9a9890b66c3a
Ryan Wilkinson <charlesryanwilkinson@gmail.com>
2017-04-29 13:27:59 -0400

    seeds for cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+City.create({name: "Atlanta", photo_url: "https://cdn.oncarrot.com/uploads/sites/15904/2017/03/atlanta-carrot-pic1.jpg"})
+City.create({name: "San Francisco", photo_url: "http://images.trvl-media.com/media/content/shared/images/travelguides/San-Francisco-and-vicinity-178305-smallTabletRetina.jpg"})
+City.create({name: "London", photo_url: "https://static01.nyt.com/images/2015/12/09/travel/09intransitphoto-london/09intransitphoto-london-facebookJumbo.jpg"})
