24629631bae625832ad0de3fa28be1e83a356ed1
Ryan Wilkinson <charlesryanwilkinson@gmail.com>
2017-05-01 14:12:49 -0400

    defualt profile pic

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,5 +7,6 @@ class CitiesController < ApplicationController
     @city = City.find(params[:id])
     @posts = City.find(params[:id]).posts
     @posts = @posts.paginate(:page => params[:page], :per_page => 10)
+    @posts = @posts.order('created_at DESC')
   end
 end
