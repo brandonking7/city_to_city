783917a669eae97d1bf0307d50a0fdd3afa35127
Ryan Wilkinson <charlesryanwilkinson@gmail.com>
2017-05-01 10:27:13 -0400

    description added to city

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429180833) do
+ActiveRecord::Schema.define(version: 20170501141236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,9 @@ ActiveRecord::Schema.define(version: 20170429180833) do
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.string   "photo_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "posts", force: :cascade do |t|
